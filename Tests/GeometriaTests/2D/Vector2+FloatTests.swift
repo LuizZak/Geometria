da05df676845aeb40b25c4adfa2eaede18bbe666
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-08-21 19:21:49 -0300

    Removing unused Vector2<Float> specialization code

--- Tests/GeometriaTests/2D/Vector2+FloatTests.swift
+++ Tests/GeometriaTests/2D/Vector2+FloatTests.swift
@@ -4,52 +4,6 @@ import Geometria
 class Vector2_FloatTests: XCTestCase {
     private let accuracy: Float = 1.0e-6
     
-    func testNormalized() {
-        let vec = Vector2F(x: -10, y: 20)
-        
-        assertEqual(vec.normalized(), Vector2F(x: -0.44721362, y: 0.89442724), accuracy: 1e-7)
-    }
-    
-    func testDot() {
-        let v1 = Vector2F(x: 10, y: 20)
-        let v2 = Vector2F(x: 30, y: 40)
-        
-        XCTAssertEqual(v1.dot(v2), 1100.0)
-    }
-    
-    func testDistanceTo() {
-        let v1 = Vector2F(x: 10, y: 20)
-        let v2 = Vector2F(x: 30, y: 40)
-        
-        XCTAssertEqual(v1.distance(to: v2), 28.284271247461902)
-    }
-    
-    func testDistanceTo_zeroDistance() {
-        let vec = Vector2F(x: 10, y: 20)
-        
-        XCTAssertEqual(vec.distance(to: vec), 0.0)
-    }
-    
-    func testDistanceSquared() {
-        let v1 = Vector2F(x: 10, y: 20)
-        let v2 = Vector2F(x: 30, y: 40)
-        
-        XCTAssertEqual(v1.distanceSquared(to: v2), 800.0)
-    }
-    
-    func testDistanceSquared_zeroDistance() {
-        let vec = Vector2F(x: 10, y: 20)
-        
-        XCTAssertEqual(vec.distanceSquared(to: vec), 0.0)
-    }
-    
-    func testCross() {
-        let v1 = Vector2F(x: 10, y: 20)
-        let v2 = Vector2F(x: 30, y: 40)
-        
-        XCTAssertEqual(v1.cross(v2), -200.0)
-    }
-    
     func testSimdMatrix_identity() {
         let matrix = Vector2F.simdMatrix()
         
