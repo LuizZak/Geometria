8a5c49c72ea6ed6814001c89fce517c5ed8eb167
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-08-24 10:30:16 -0300

    Correcting Line.project

--- Sources/Geometria/Generalized/Line.swift
+++ Sources/Geometria/Generalized/Line.swift
@@ -27,22 +27,38 @@ public extension Line where Vector: VectorMultiplicative {
     }
 }
 
-public extension Line where Vector: VectorReal {
-    /// Returns the length of this line
+public extension Line where Vector: VectorDivisible {
+    /// Performs a vector projection of a given vector with respect to this line,
+    /// returning a scalar value representing the normalized magnitude of the
+    /// projected point between `start <-> end`.
+    ///
+    /// By multiplying the result of this function by `start + (end - start)`,
+    /// the projected point as it lays on this line can be obtained.
     @inlinable
-    var length: Scalar {
-        return (end - start).length
+    func projectScalar(_ vector: Vector) -> Scalar {
+        let relEnd = end - start
+        let relVec = vector - start
+        
+        let proj = relVec.dot(relEnd) / lengthSquared
+        
+        return proj
     }
     
     /// Performs a vector projection of a given vector with respect to this line.
     /// The resulting vector lies within the infinite line formed by
     /// `start <-> end`, extending past both ends.
+    @inlinable
     func project(_ vector: Vector) -> Vector {
-        let relEnd = end - start
-        let relVec = vector - start
-        
-        let proj = relVec.dot(relEnd) / length
+        let proj = projectScalar(vector)
         
         return start + (end - start) * proj
+    }
+}
+
+public extension Line where Vector: VectorReal {
+    /// Returns the length of this line
+    @inlinable
+    var length: Scalar {
+        return (end - start).length
     }
 }
--- Tests/GeometriaTests/Generalized/LineTests.swift
+++ Tests/GeometriaTests/Generalized/LineTests.swift
@@ -62,13 +62,49 @@ extension LineTests {
     }
 }
 
-// MARK: VectorReal Conformance
+// MARK: VectorDivisible Conformance
 
 extension LineTests {
-    func testLength() {
-        let sut = Line(start: .init(x: 1, y: 2), end: .init(x: 3, y: 5))
+    func testProjectScalar2D() {
+        let sut = Line(x1: 2, y1: 1, x2: 3, y2: 2)
+        let point = Vector2D(x: 2, y: 2)
         
-        XCTAssertEqual(sut.length, 3.605551275463989, accuracy: 1e-13)
+        XCTAssertEqual(sut.projectScalar(point), 0.5, accuracy: 1e-12)
+    }
+    
+    func testProjectScalar2D_offBounds() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 0)
+        let point = Vector2D(x: -2, y: 2)
+        
+        XCTAssertEqual(sut.projectScalar(point), -2)
+    }
+    
+    func testProjectScalar2D_skewed() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 1)
+        let point = Vector2D(x: 0, y: 2)
+        
+        XCTAssertEqual(sut.projectScalar(point), 1, accuracy: 1e-12)
+    }
+    
+    func testProjectScalar3D() {
+        let sut = Line3D(x1: 0, y1: 0, z1: 0, x2: 3, y2: 0, z2: 0)
+        let point = Vector3D(x: 1, y: 1, z: 0)
+        
+        XCTAssertEqual(sut.projectScalar(point), 0.3333333333333333, accuracy: 1e-12)
+    }
+    
+    func testProjectScalar3D_offBounds() {
+        let sut = Line3D(x1: 0, y1: 0, z1: 0, x2: 1, y2: 0, z2: 0)
+        let point = Vector3D(x: -3, y: 1, z: 0)
+        
+        XCTAssertEqual(sut.projectScalar(point), -3)
+    }
+    
+    func testProjectScalar3D_skewed() {
+        let sut = Line3D(x1: 0, y1: 0, z1: 0, x2: 1, y2: 1, z2: 1)
+        let point = Vector3D(x: 0, y: 2, z: 0)
+        
+        XCTAssertEqual(sut.projectScalar(point), 0.6666666666666666, accuracy: 1e-12)
     }
     
     func testProject2D() {
@@ -76,7 +112,16 @@ extension LineTests {
         let point = Vector2D(x: 2, y: 2)
         
         assertEqual(sut.project(point),
-                    Vector2D(x: 2.7071067811865475, y: 1.7071067811865475),
+                    Vector2D(x: 2.5, y: 1.5),
+                    accuracy: 1e-12)
+    }
+    
+    func testProject2D_parallel() {
+        let sut = Line2D(x1: 0, y1: 0, x2: 3, y2: 0)
+        let point = Vector2D(x: 1, y: 0)
+        
+        assertEqual(sut.project(point),
+                    Vector2D(x: 1, y: 0),
                     accuracy: 1e-12)
     }
     
@@ -92,16 +137,25 @@ extension LineTests {
         let point = Vector2D(x: 0, y: 2)
         
         assertEqual(sut.project(point),
-                    Vector2D(x: 1.414213562373095, y: 1.414213562373095),
+                    Vector2D(x: 1, y: 1),
                     accuracy: 1e-12)
     }
     
-    func testProject3D() {
-        let sut = Line3D(x1: 0, y1: 0, z1: 0, x2: 1, y2: 0, z2: 0)
-        let point = Vector3D(x: 3, y: 1, z: 0)
+    func testProject2D_skewed_centered() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 1)
+        let point = Vector2D(x: 0, y: 1)
         
         assertEqual(sut.project(point),
-                    Vector3D(x: 3, y: 0, z: 0),
+                    Vector2D(x: 0.5, y: 0.5),
+                    accuracy: 1e-12)
+    }
+    
+    func testProject3D_parallel() {
+        let sut = Line3D(x1: 0, y1: 0, z1: 0, x2: 3, y2: 0, z2: 0)
+        let point = Vector3D(x: 1, y: 0, z: 0)
+        
+        assertEqual(sut.project(point),
+                    Vector3D(x: 1, y: 0, z: 0),
                     accuracy: 1e-12)
     }
     
@@ -117,7 +171,26 @@ extension LineTests {
         let point = Vector3D(x: 0, y: 2, z: 0)
         
         assertEqual(sut.project(point),
-                    Vector3D(x: 1.1547005383792517, y: 1.1547005383792517, z: 1.1547005383792517),
+                    Vector3D(x: 0.6666666666666666, y: 0.6666666666666666, z: 0.6666666666666666),
                     accuracy: 1e-12)
+    }
+    
+    func testProject3D_skewed_centered() {
+        let sut = Line3D(x1: 0, y1: 0, z1: 0, x2: 1, y2: 1, z2: 1)
+        let point = Vector3D(x: 1, y: 1, z: 0)
+        
+        assertEqual(sut.project(point),
+                    Vector3D(x: 0.6666666666666666, y: 0.6666666666666666, z: 0.6666666666666666),
+                    accuracy: 1e-12)
+    }
+}
+
+// MARK: VectorReal Conformance
+
+extension LineTests {
+    func testLength() {
+        let sut = Line(start: .init(x: 1, y: 2), end: .init(x: 3, y: 5))
+        
+        XCTAssertEqual(sut.length, 3.605551275463989, accuracy: 1e-13)
     }
 }
