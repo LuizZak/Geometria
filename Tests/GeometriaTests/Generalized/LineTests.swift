d0660ea6f1c61ee85692b54c2fdf65c8032d4b37
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-08-24 10:38:38 -0300

    Adding unit tests for capping behavior of Line.distance(to:)

--- Tests/GeometriaTests/Generalized/LineTests.swift
+++ Tests/GeometriaTests/Generalized/LineTests.swift
@@ -201,6 +201,20 @@ extension LineTests {
         XCTAssertEqual(sut.distance(to: point), 0.7071067811865476, accuracy: 1e-15)
     }
     
+    func testDistanceTo2D_pastStart() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 1)
+        let point = Vector2D(x: -1, y: 0)
+        
+        XCTAssertEqual(sut.distance(to: point), 1, accuracy: 1e-15)
+    }
+    
+    func testDistanceTo2D_pastEnd() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 1)
+        let point = Vector2D(x: 1, y: 2)
+        
+        XCTAssertEqual(sut.distance(to: point), 1, accuracy: 1e-15)
+    }
+    
     func testDistanceTo3D() {
         let sut = Line3D(x1: 0, y1: 0, z1: 0, x2: 1, y2: 1, z2: 1)
         let point = Vector3D(x: 1, y: 1, z: 0)
