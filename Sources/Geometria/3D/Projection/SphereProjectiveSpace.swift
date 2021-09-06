39365534c0b856c5984f117f25bea64aa8aefcd4
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-09-06 07:42:45 -0300

    Correcting another typo in doc comments

--- Sources/Geometria/3D/Projection/SphereProjectiveSpace.swift
+++ Sources/Geometria/3D/Projection/SphereProjectiveSpace.swift
@@ -2,7 +2,8 @@ import RealModule
 
 /// A projected space laying on the surface of a 3-dimensional sphere, with a
 /// 2-dimensional underlying space where vectors represent the azimuth (XY angle
-/// of the points) and elevation (the angle between the  on the sphere.
+/// of the points) and elevation (the angle between the point and the XY-plane
+/// on the sphere).
 public protocol SphereProjectiveSpace: ProjectiveSpace where Vector: Vector3Type, Vector.Scalar == Scalar, Coordinates == SphereCoordinates<Scalar> {
     associatedtype Scalar: Real
 }
