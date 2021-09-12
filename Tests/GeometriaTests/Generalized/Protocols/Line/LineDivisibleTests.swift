f0949a587c6c22f2ef30d7eb8dc3c84e167bdd12
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-09-11 23:38:27 -0300

    Moving unit tests due to last commit

---
+++ Tests/GeometriaTests/Generalized/Protocols/Line/LineDivisibleTests.swift
@@ -0,0 +1,35 @@
+import XCTest
+import Geometria
+
+class LineDivisibleTests: XCTestCase {
+    typealias Line = Line2D
+    typealias Line3 = Line3D
+    
+    func testDistanceSquaredTo2D() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 1)
+        let point = Vector2D(x: 0, y: 1)
+        
+        XCTAssertEqual(sut.distanceSquared(to: point), 0.5, accuracy: 1e-15)
+    }
+    
+    func testDistanceSquaredTo2D_pastStart() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 1)
+        let point = Vector2D(x: -1, y: 0)
+        
+        XCTAssertEqual(sut.distanceSquared(to: point), 0.5, accuracy: 1e-15)
+    }
+    
+    func testDistanceSquaredTo2D_pastEnd() {
+        let sut = Line(x1: 0, y1: 0, x2: 1, y2: 1)
+        let point = Vector2D(x: 1, y: 3)
+        
+        XCTAssertEqual(sut.distanceSquared(to: point), 2, accuracy: 1e-15)
+    }
+    
+    func testDistanceSquaredTo3D() {
+        let sut = Line3(x1: 0, y1: 0, z1: 0, x2: 1, y2: 1, z2: 1)
+        let point = Vector3D(x: 1, y: 1, z: 0)
+        
+        XCTAssertEqual(sut.distanceSquared(to: point), 0.6666666666666667, accuracy: 1e-15)
+    }
+}
