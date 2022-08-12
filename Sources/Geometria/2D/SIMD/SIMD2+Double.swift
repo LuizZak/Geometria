1ca80715d4ce194c94b27dc622fe3f3ad621a206
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-08-12 17:53:06 -0300

    Fixing SIMD support

--- Sources/Geometria/2D/SIMD/SIMD2+Double.swift
+++ Sources/Geometria/2D/SIMD/SIMD2+Double.swift
@@ -19,14 +19,7 @@ extension SIMD2: VectorTakeable where Scalar == Double {
 }
 
 extension SIMD2: Vector2Type where Scalar == Double {
-    public typealias SubVector3 = SIMD3<Scalar>
-    public typealias SubVector4 = SIMD4<Scalar>
-
-    /// Defines the dimension of an indexed takeable getter.
-    public enum TakeDimensions: Int {
-        case x
-        case y
-    }
+    
 }
 
 extension SIMD2: VectorComparable where Scalar == Double {
