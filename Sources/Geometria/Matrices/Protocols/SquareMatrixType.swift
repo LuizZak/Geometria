7e9d3908aa551e15d8c3616cbbf82b80688e2918
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-11-13 02:13:47 -0300

    Adding protocol TransposableMatrixType

--- Sources/Geometria/Matrices/Protocols/SquareMatrixType.swift
+++ Sources/Geometria/Matrices/Protocols/SquareMatrixType.swift
@@ -2,16 +2,11 @@
 /// columns.
 ///
 /// [square matrices]: https://en.wikipedia.org/wiki/Square_matrix
-public protocol SquareMatrixType: MatrixType {
+public protocol SquareMatrixType: TransposableMatrixType where Transpose == Self {
     /// Returns the [determinant] of this square matrix.
     ///
     /// [determinant]: https://en.wikipedia.org/wiki/Determinant
     func determinant() -> Scalar
-    
-    /// Returns a new matrix that is a [transposition] of this matrix.
-    ///
-    /// [transposition]: https://en.wikipedia.org/wiki/Transpose
-    func transposed() -> Self
     
     /// Returns the [inverse of this matrix](https://en.wikipedia.org/wiki/Invertible_matrix).
     ///
