cc1296e5c80ce09ee6de7727607216098b0f20de
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2024-07-02 11:50:44 -0300

    Adding LinePolygon2.isPointOnEdge() and LinePolygon.project()

--- Tests/GeometriaTests/Generalized/LinePolygonTests.swift
+++ Tests/GeometriaTests/Generalized/LinePolygonTests.swift
@@ -4,7 +4,7 @@ import TestCommons
 
 class LinePolygonTests: XCTestCase {
     typealias LinePolygon = LinePolygon2D
-    
+
     func testCodable() throws {
         let sut = LinePolygon(vertices: [
             .init(x: 0, y: 0),
@@ -13,47 +13,47 @@ class LinePolygonTests: XCTestCase {
         ])
         let encoder = JSONEncoder()
         let decoder = JSONDecoder()
-        
+
         let data = try encoder.encode(sut)
         let result = try decoder.decode(LinePolygon.self, from: data)
-        
+
         XCTAssertEqual(sut, result)
     }
-    
+
     func testEquals() {
         XCTAssertEqual(LinePolygon(), LinePolygon())
         XCTAssertEqual(LinePolygon(vertices: [.init(x: 0, y: 0)]),
                        LinePolygon(vertices: [.init(x: 0, y: 0)]))
     }
-    
+
     func testUnequals() {
         XCTAssertNotEqual(LinePolygon(vertices: []),
                           LinePolygon(vertices: [.init(x: 0, y: 0)]))
-        
+
         XCTAssertNotEqual(LinePolygon(vertices: [.init(x: 999, y: 0)]),
                           LinePolygon(vertices: [.init(x: 0, y: 0)]))
-        
+
         XCTAssertNotEqual(LinePolygon(vertices: [.init(x: 0, y: 999)]),
                           LinePolygon(vertices: [.init(x: 0, y: 0)]))
     }
-    
+
     func testHashable() {
         XCTAssertNotEqual(LinePolygon(vertices: []).hashValue,
                           LinePolygon(vertices: [.init(x: 0, y: 0)]).hashValue)
-        
+
         XCTAssertNotEqual(LinePolygon(vertices: [.init(x: 999, y: 0)]).hashValue,
                           LinePolygon(vertices: [.init(x: 0, y: 0)]).hashValue)
-        
+
         XCTAssertNotEqual(LinePolygon(vertices: [.init(x: 0, y: 999)]).hashValue,
                           LinePolygon(vertices: [.init(x: 0, y: 0)]).hashValue)
     }
-    
+
     func testAddVertex() {
         var sut = LinePolygon(vertices: [])
-        
+
         sut.addVertex(.init(x: 1, y: 2))
         sut.addVertex(.init(x: 3, y: 5))
-        
+
         XCTAssertEqual(sut.vertices, [
             .init(x: 1, y: 2),
             .init(x: 3, y: 5),
@@ -102,18 +102,18 @@ extension LinePolygonTests {
             .init(x: 4, y: 1),
             .init(x: 3, y: -4),
         ])
-        
+
         let result = sut.bounds
-        
+
         XCTAssertEqual(result.minimum, .init(x: -2, y: -4))
         XCTAssertEqual(result.maximum, .init(x: 4, y: 3))
     }
-    
+
     func testBounds_empty() {
         let sut = LinePolygon()
-        
+
         let result = sut.bounds
-        
+
         XCTAssertEqual(result.minimum, .zero)
         XCTAssertEqual(result.maximum, .zero)
     }
@@ -124,18 +124,18 @@ extension LinePolygonTests {
 extension LinePolygonTests {
     func testAverage_emptyVertices() {
         let sut = LinePolygon(vertices: [])
-        
+
         XCTAssertEqual(sut.average, .zero)
     }
-    
+
     func testAverage_singleVertex() {
         let sut = LinePolygon(vertices: [
             .init(x: 1, y: 2),
         ])
-        
+
         XCTAssertEqual(sut.average, .init(x: 1, y: 2))
     }
-    
+
     func testAverage_multiVertices() {
         let sut = LinePolygon(vertices: [
             .init(x: 1, y: 2),
@@ -143,7 +143,41 @@ extension LinePolygonTests {
             .init(x: 7, y: 11),
             .init(x: 13, y: 17),
         ])
-        
+
         XCTAssertEqual(sut.average, .init(x: 6, y: 8.75))
+    }
+
+    func testProject() {
+        let sut = LinePolygon(vertices: [
+            .init(x: 0, y: 0),
+            .init(x: 1, y: 1),
+            .init(x: 2, y: 0),
+            .init(x: 2, y: 2),
+            .init(x: 0, y: 2),
+        ])
+        let test1 = Vector2D(x: 0, y: 0)
+        let test2 = Vector2D(x: 1, y: 1)
+        let test3 = Vector2D(x: 2, y: 1)
+        let test4 = Vector2D(x: 1, y: 1.5)
+        let test5 = Vector2D(x: 2.32, y: 2.32)
+
+        TestFixture.beginFixture(sceneScale: 100, renderScale: 50) { fixture in
+            fixture.add(sut)
+
+            fixture.assertions(on: test1)
+                .assertEquals(sut.project(test1), .init(x: 0.0, y: 0.0))
+
+            fixture.assertions(on: test2)
+                .assertEquals(sut.project(test2), .init(x: 1.0, y: 1.0))
+
+            fixture.assertions(on: test3)
+                .assertEquals(sut.project(test3), .init(x: 2.0, y: 1.0))
+
+            fixture.assertions(on: test4)
+                .assertEquals(sut.project(test4), .init(x: 1.0, y: 1.0))
+
+            fixture.assertions(on: test5)
+                .assertEquals(sut.project(test5), .init(x: 2.0, y: 2.0))
+        }
     }
 }
