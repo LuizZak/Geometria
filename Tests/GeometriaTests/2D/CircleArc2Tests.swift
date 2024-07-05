16d71e90fa3a0a98e68c57a4b69c3053b1f97919
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2024-07-05 13:06:51 -0300

    Adding angle sweeping unit tests to CircleArc2

--- Tests/GeometriaTests/2D/CircleArc2Tests.swift
+++ Tests/GeometriaTests/2D/CircleArc2Tests.swift
@@ -103,6 +103,80 @@ class CircleArc2Tests: XCTestCase {
         XCTAssertFalse(sut.contains((.pi / 4 - .pi / 2) - 0.01)) // .nextDown rounds up due to floating-point imprecision
         XCTAssertFalse(sut.contains(Double.pi))
     }
+
+    func testContainsAngle_overSweep_scanning() {
+        var failureCount = 0
+        func assertTrue(_ value: Bool, line: UInt = #line) {
+            guard !value else { return }
+
+            guard failureCount < 10 else { return }
+            defer { failureCount += 1 }
+
+            XCTAssertTrue(value, line: line)
+        }
+        func assertFalse(_ value: Bool, line: UInt = #line) {
+            guard value else { return }
+
+            guard failureCount < 10 else { return }
+            defer { failureCount += 1 }
+
+            XCTAssertFalse(value, line: line)
+        }
+
+        let sweepAngle = Double.pi * 1.8
+        for startAngle in stride(from: 0.0, through: .pi, by: 0.01) {
+            let sut = makeSut(
+                center: .zero,
+                radius: 1.0,
+                startAngle: startAngle,
+                sweepAngle: sweepAngle
+            )
+
+            assertFalse(sut.contains(startAngle - 0.01))
+            assertTrue(sut.contains(startAngle))
+            assertTrue(sut.contains(startAngle + sweepAngle / 2))
+            assertTrue(sut.contains(startAngle + sweepAngle))
+            assertFalse(sut.contains(startAngle + sweepAngle + 0.01))
+        }
+    }
+
+    func testContainsAngle_negativeSweep_scanning() {
+        var failureCount = 0
+        func assertTrue(_ value: Bool, line: UInt = #line) {
+            guard !value else { return }
+
+            guard failureCount < 10 else { return }
+            defer { failureCount += 1 }
+
+            XCTAssertTrue(value, line: line)
+        }
+        func assertFalse(_ value: Bool, line: UInt = #line) {
+            guard value else { return }
+
+            guard failureCount < 10 else { return }
+            defer { failureCount += 1 }
+
+            XCTAssertFalse(value, line: line)
+        }
+
+        let sweepAngle = -Double.pi * 1.8
+        for startAngle in stride(from: 0.0, through: .pi, by: 0.01) {
+            let sut = makeSut(
+                center: .zero,
+                radius: 1.0,
+                startAngle: startAngle,
+                sweepAngle: sweepAngle
+            )
+
+            assertFalse(sut.contains(startAngle + 0.01))
+            assertTrue(sut.contains(startAngle - 0.01))
+            assertTrue(sut.contains(startAngle))
+            assertTrue(sut.contains(startAngle + sweepAngle / 2))
+            assertTrue(sut.contains(startAngle + sweepAngle))
+            assertTrue(sut.contains(startAngle + sweepAngle + 0.01))
+            assertFalse(sut.contains(startAngle + sweepAngle - 0.01))
+        }
+    }
 }
 
 // MARK: - Test internals
