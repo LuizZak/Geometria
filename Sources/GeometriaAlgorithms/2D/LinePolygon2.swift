06a16b7ca6d4c558fe9c27401ccff5edd79ac7ab
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2024-07-10 15:39:45 -0300

    Adding point-cloud type

---
+++ Sources/GeometriaAlgorithms/2D/LinePolygon2.swift
@@ -0,0 +1,8 @@
+import Geometria
+
+public extension LinePolygon2 {
+    /// Returns a convex hull of the points represented by this line polygon.
+    func kirkpatrickSeidelConvexHull() -> Self {
+        self
+    }
+}
